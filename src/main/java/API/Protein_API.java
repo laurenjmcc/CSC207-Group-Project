96fea8a3c4b93898eea66b0a9c83ad1dbfb6fe33
Haroon Shah <haroon.shah@mail.utoronto.ca>
2024-11-10 15:07:06 -0500

    API

--- src/main/java/API/Protein_API.java
+++ src/main/java/API/Protein_API.java
@@ -10,44 +10,36 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class Protein_API {
-    public static void main(String[] args) throws Exception {
-        String requestURL = "https://www.ebi.ac.uk/proteins/api/proteins/interaction/P04637";
-        URL url = new URL(requestURL);
-
-        URLConnection connection = url.openConnection();
-        HttpURLConnection httpConnection = (HttpURLConnection)connection;
-
-        httpConnection.setRequestProperty("Accept", "application/xml");
-
-
-        InputStream response = connection.getInputStream();
-        int responseCode = httpConnection.getResponseCode();
-
-        if(responseCode != 200) {
-            throw new RuntimeException("Response code was not 200. Detected response was "+responseCode);
-        }
-
-        String output;
-        Reader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(response, "UTF-8"));
-            StringBuilder builder = new StringBuilder();
-            char[] buffer = new char[8192];
-            int read;
-            while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
-                builder.append(buffer, 0, read);
+        public static void main(String[] args) throws Exception {
+            String requestURL = "https://www.ebi.ac.uk/proteins/api/proteins/interaction/P04637";
+            URL url = new URL(requestURL);
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection)connection;
+            httpConnection.setRequestProperty("Accept", "application/json");
+            InputStream response = connection.getInputStream();
+            int responseCode = httpConnection.getResponseCode();
+            if(responseCode != 200) {
+                throw new RuntimeException("Response code was not 200. Detected response was "+responseCode);
             }
-            output = builder.toString();
-        }
-        finally {
-            if (reader != null) try {
-                reader.close();
-            } catch (IOException logOrIgnore) {
-                logOrIgnore.printStackTrace();
+            String output;
+            Reader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(response, "UTF-8"));
+                StringBuilder builder = new StringBuilder();
+                char[] buffer = new char[8192];
+                int read;
+                while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+                    builder.append(buffer, 0, read);
+                }
+                output = builder.toString();
             }
+            finally {
+                if (reader != null) try {
+                    reader.close();
+                } catch (IOException logOrIgnore) {
+                    logOrIgnore.printStackTrace();
+                }
+            }
+            System.out.println(output);
         }
-
-        System.out.println(output);
-    }
-
 }
