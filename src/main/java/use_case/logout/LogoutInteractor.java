72c50729790a42d574c9fffd35905914aa16527b
ArhanUofT <123514345+ArhanUofT@users.noreply.github.com>
2024-11-10 14:38:10 -0500

    Add the signup/login engine to out project repo

---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,29 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+        // Which parameter is the DAO and which is the presenter?
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        final String username = this.userDataAccessObject.getCurrentUsername();
+        this.userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
+        logoutPresenter.prepareSuccessView(logoutOutputData);
+        // * get the username out of the input data,
+        // * set the username to null in the DAO
+        // * instantiate the `LogoutOutputData`, which needs to contain the username.
+        // * tell the presenter to prepare a success view.
+    }
+}
+
