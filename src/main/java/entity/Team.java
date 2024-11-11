3f322a8e9eb0c111fa8129d19e06ecbe89187520
laurenjmcc <lauren.mccaughey@mail.utoronto.ca>
2024-11-11 11:57:42 -0500

    ALL CHANGES IN ENTITY PACKAGE. Created a Team entity, updated the user.interface and commonuser class to add the variables and team methods for the team class. (Mainly, getTeamName, addTeamname)

---
+++ src/main/java/entity/Team.java
@@ -0,0 +1,27 @@
+package entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Team {
+    private final String teamName;
+    private final Set<String> memberUsernames;
+
+
+    public Team(String teamname, Set<String> memberUsernames) {
+        this.teamName = teamname;
+        this.memberUsernames = new HashSet<>();
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public Set<String> getMemberUsernames() {
+        return memberUsernames;
+    }
+
+    public void addMember(String username) {
+        memberUsernames.add(username);
+    }
+}
