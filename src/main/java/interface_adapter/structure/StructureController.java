e260e96c9d049e662ee4529380b55f8463830f45
saraelashry <sara.elashry1809@gmail.com>
2024-11-29 17:57:12 -0500

    Fixed the error for StructureController

--- src/main/java/interface_adapter/structure/StructureController.java
+++ src/main/java/interface_adapter/structure/StructureController.java
@@ -1,7 +1,17 @@
 package interface_adapter.structure;
 
+import interface_adapter.signup.SignupController;
+import use_case.structure.StructureInputBoundary;
+
 public class StructureController {
+    private final StructureInputBoundary structureUseCaseInteractor;
+
+    public StructureController(StructureInputBoundary structureUseCaseInteractor) {
+        this.structureUseCaseInteractor = structureUseCaseInteractor;
+    }
+
     public void execute() {
         structureUseCaseInteractor.execute();
     }
 }
+
--- src/main/java/use_case/structure/StructureInputBoundary.java
+++ src/main/java/use_case/structure/StructureInputBoundary.java
@@ -1,4 +1,10 @@
 package use_case.structure;
 
 public interface StructureInputBoundary {
+    /**
+     * Executes the signup use case.
+     * @param structureInputData the input data
+     */
+    void execute(StructureInputData structureInputData);
 }
+
